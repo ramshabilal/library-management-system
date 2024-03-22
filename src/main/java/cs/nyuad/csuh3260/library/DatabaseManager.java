45cad0c92ce7c56a7fa0e7c5650902a8a2fd9f89
Ramsha Bilal <rb4919@nyu.edu>
2024-03-22 23:22:27 +0400

    RED: add failing test for getUsers() in database manager class

--- src/main/java/cs/nyuad/csuh3260/library/DatabaseManager.java
+++ src/main/java/cs/nyuad/csuh3260/library/DatabaseManager.java
@@ -1,5 +1,53 @@
 package cs.nyuad.csuh3260.library;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+// import com.mongodb.client.result.DeleteResult;
+// import com.mongodb.internal.bulk.DeleteRequest;
+
+import org.bson.Document;
+
 public class DatabaseManager {
+
+    private static final String DATABASE_NAME = "library_g3";
+    private static final String COLLECTION_NAME = "users";
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> usersCollection;
+
+    public DatabaseManager() {
+        String uri = "mongodb+srv://ramshabilal:RsRRPoY9gZCVNjhi@cluster0.siam2zv.mongodb.net/library_g3?retryWrites=true&w=majority&appName=Cluster0";
+        this.mongoClient = MongoClients.create(uri);
+        this.database = mongoClient.getDatabase(DATABASE_NAME);
+        this.usersCollection = database.getCollection(COLLECTION_NAME);
+    }
+
+    public DatabaseManager(MongoDatabase database) {
+        String uri = "mongodb+srv://ramshabilal:RsRRPoY9gZCVNjhi@cluster0.siam2zv.mongodb.net/library_g3?retryWrites=true&w=majority&appName=Cluster0";
+        this.mongoClient = MongoClients.create(uri);
+        this.database = database;
+        this.usersCollection = database.getCollection(COLLECTION_NAME);
+    }
+
+
+    public void addUser(User user) {
+        
+    }
+
+    public List<User> getUsers() {
+        return null;
+    }
+
+    // Close MongoDB client
+    public void close() {
+        mongoClient.close();
+    }
 
 }
--- src/test/java/cs/nyuad/csuh3260/library/DatabaseManagerTest.java
+++ src/test/java/cs/nyuad/csuh3260/library/DatabaseManagerTest.java
@@ -1,7 +1,51 @@
+package cs.nyuad.csuh3260.library;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.Doc;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
 
 public class DatabaseManagerTest {
+
+    private DatabaseManager databaseManager;
+    private MongoDatabase mockDatabase;
+    private MongoCollection<Document> mockUsersCollection;
+
+    @BeforeEach
+    public void setUp() {
+        databaseManager = new DatabaseManager();
+    }
+
+    @Test
+    public void testGetUsers() {
+        // When
+        List<User> users = databaseManager.getUsers();
+
+        // Then
+        assertNotNull(users);
+        assertTrue(users.isEmpty());
+    }
 
 }

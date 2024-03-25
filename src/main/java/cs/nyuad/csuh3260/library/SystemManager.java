5dca3fd3b5c10f68fb2f50d0e2408e66d2380c81
Tuqa <tuqaabdelnasir@gmail.com>
2024-03-25 23:56:38 +0400

    changes to tests

--- src/main/java/cs/nyuad/csuh3260/library/SystemManager.java
+++ src/main/java/cs/nyuad/csuh3260/library/SystemManager.java
@@ -1,5 +1,6 @@
 package cs.nyuad.csuh3260.library;
 
+import java.io.PrintStream;
 import java.util.*;
 
 public class SystemManager {
@@ -7,13 +8,58 @@ public class SystemManager {
     private Map<String, List<String>> bookings;
     private User curUser;
     private Map<String, Integer> availabilityList;
+    private Scanner scanner;
+    private PrintStream systemOut;
 
     public SystemManager(DatabaseManager databaseManager) {
         this.databaseManager = databaseManager;
         this.bookings = new HashMap<>();
         this.availabilityList = new HashMap<>();
         initializeAvailabilityList();
+        scanner = new Scanner(System.in);
+        systemOut = System.out;
     }
+
+    public SystemManager(DatabaseManager databaseManager, Scanner scanner) {
+        this.databaseManager = databaseManager;
+        this.bookings = new HashMap<>();
+        this.availabilityList = new HashMap<>();
+        initializeAvailabilityList();
+        this.scanner = scanner;
+        systemOut = System.out;
+    }
+
+    public SystemManager(DatabaseManager databaseManager, Scanner scanner, PrintStream systemOut) {
+        this.databaseManager = databaseManager;
+        this.bookings = new HashMap<>();
+        this.availabilityList = new HashMap<>();
+        initializeAvailabilityList();
+        this.scanner = scanner;
+        this.systemOut = systemOut;
+    }
+
+    public SystemManager(Scanner scanner, PrintStream systemOut, DatabaseManager databaseManager, User mockedUser) {
+        this.scanner = scanner;
+        this.systemOut = systemOut;
+        this.databaseManager = databaseManager;
+        this.curUser = mockedUser;
+    }
+
+    public SystemManager() {
+        scanner = new Scanner(System.in);
+        systemOut = System.out;
+    }
+
+    public SystemManager(Scanner scanner) {
+        this.scanner = scanner;
+        systemOut = System.out;
+    }
+
+    public SystemManager(Scanner scanner, PrintStream systemOut) {
+        this.scanner = scanner;
+        this.systemOut = systemOut;
+    }
+
 
     private void initializeAvailabilityList() {
         List<Book> allBooks = databaseManager.getBooks();
@@ -23,10 +69,7 @@ public class SystemManager {
     }
 
 
-    //login, sign up,
-
     public List<Book> search(List<String> keywords) {
-        
         List<Book> matchingBooks = new ArrayList<>();
         List<Book> allBooks = databaseManager.getBooks();
         for (Book book : allBooks) {
@@ -40,10 +83,8 @@ public class SystemManager {
         return matchingBooks;
     }
 
-    
     public boolean reserve(String bookID) {
         if (availabilityList.containsKey(bookID) && availabilityList.get(bookID) > 0) {
-            // minuses 1 if available
             availabilityList.put(bookID, availabilityList.get(bookID) - 1);
             bookings.computeIfAbsent(curUser.getId(), k -> new ArrayList<>()).add(bookID);
             return true;
@@ -51,27 +92,21 @@ public class SystemManager {
         return false;
     }
 
-
     public boolean returnBook(String bookID) {
-        // Check if the book is currently borrowed by the user
         if (bookings.containsKey(curUser.getId()) && bookings.get(curUser.getId()).contains(bookID)) {
-            // Remove the book from the user's bookings
             bookings.get(curUser.getId()).remove(bookID);
-            
-            // Increase the availability count of the book
             int updatedCount = availabilityList.get(bookID) + 1;
             availabilityList.put(bookID, updatedCount);
-            
             return true;
         }
         return false;
     }
+
     public boolean addNewBook(String title, String author) {
-        // Check if a book with the same title and author already exists
         List<Book> allBooks = databaseManager.getBooks();
         for (Book book : allBooks) {
             if (book.getTitle().equals(title) && book.getAuthor().equals(author)) {
-                return false; // Book already exists
+                return false;
             }
         }
         String bookId = generateBookId();
@@ -82,9 +117,8 @@ public class SystemManager {
     }
 
     public boolean addMoreBooks(String bookID, int count) {
-        // Check if the book exists
         if (!availabilityList.containsKey(bookID)) {
-            return false; // Book does not exist
+            return false;
         }
         try {
             databaseManager.addMoreBooks(bookID, count);
@@ -94,25 +128,33 @@ public class SystemManager {
             e.printStackTrace();
             return false;
         }
-        
     }
 
     public boolean removeAllBook(String bookID) {
-         // Check if the book exists
-         if (!availabilityList.containsKey(bookID)) {
-            return false; // Book does not exist
+        if (!availabilityList.containsKey(bookID)) {
+            return false;
         }
         databaseManager.removeAllBook(bookID);
         availabilityList.remove(bookID);
         return true;
-        
     }
 
-    public void removeKBooks(String bookID, int count) {
-        
+    public boolean removeKBooks(String bookID, int count) {
+        if (!availabilityList.containsKey(bookID) || availabilityList.get(bookID) < count) {
+            return false;
+        }
+        try {
+            databaseManager.removeKBooks(bookID, count);
+            int availableCount = availabilityList.get(bookID);
+            availabilityList.put(bookID, availableCount - count);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    private String generateBookId() {
+    public String generateBookId() {
         List<Book> allBooks = databaseManager.getBooks();
         int maxId = 0;
         for (Book book : allBooks) {
@@ -136,4 +178,211 @@ public class SystemManager {
         this.curUser = curUser;
     }
 
+    public boolean login(String username, String password) {
+        List<User> users = databaseManager.getUsers();
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                curUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean signup(String name, String username, String password) {
+        List<User> users = databaseManager.getUsers();
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return false; // User with the same username already exists
+            }
+        }
+        User newUser = new User(name, username, password);
+        databaseManager.addUser(newUser);
+        curUser = newUser;
+        return true;
+    }
+
+    public void run() {
+        systemOut.println("Hello! This is a Library program.");
+        systemOut.println("In order to use Library services, please signup or login.");
+        systemOut.println("Available methods:");
+        systemOut.println("Login: login <username>,<password>");
+        systemOut.println("Signup: signup <name>,<username>,<password>");
+        systemOut.println("exit: exit");
+
+        while (true) {
+            systemOut.print("Enter your command: ");
+            String userInput = scanner.nextLine().trim();
+
+            String[] parts = userInput.split(" ", 2);
+            String command = parts[0];
+
+            if (command.equals("login") || command.equals("signup")) {
+                String[] data = parts[1].split(",");
+                if ((command.equals("login") && data.length != 2) ||
+                        (command.equals("signup") && data.length != 3)) {
+                    systemOut.println("Invalid number of arguments. Please try again.");
+                    continue;
+                }
+                if (command.equals("login")) {
+                    String username = data[0];
+                    String password = data[1];
+                    if (login(username, password)) {
+                        break;
+                    }
+                } else {
+                    String name = data[0];
+                    String username = data[1];
+                    String password = data[2];
+                    if (signup(name, username, password)) {
+                        break;
+                    }
+                }
+            } else if (command.equals("exit")) {
+                systemOut.println("Bye!");
+                scanner.close();
+                return;
+            } else {
+                systemOut.println("Invalid command. Please try again.");
+            }
+        }
+
+        systemOut.println("Successfully authenticated! You are in. Here are available services: ");
+        boolean isAdmin = curUser.isAdmin();
+        if (isAdmin) {
+            adminProgram();
+        } else {
+            userProgram();
+        }
+    }
+
+    public void adminProgram() {
+        systemOut.println("--- Admin side ---");
+        systemOut.println("addBook: addBook <title>,<author>");
+        systemOut.println("addMoreBooks: addMoreBooks <bookID>,<count>");
+        systemOut.println("search: search <keyword1>,<keyword2>,...,<keywordN>");
+        systemOut.println("removeAllBook: removeAllBook <bookID>");
+        systemOut.println("removeKBooks: removeKBooks <bookID>,<count>");
+        systemOut.println("exit: exit");
+
+        while (true) {
+            systemOut.print("Enter your command: ");
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.equals("exit")) {
+                systemOut.println("Bye!");
+                scanner.close();
+                return;
+            }
+
+            String[] parts = userInput.split(" ", 2);
+            if (parts.length != 2) {
+                systemOut.println("Invalid command. Please try again.");
+                continue;
+            }
+            String command = parts[0];
+            String dataPart = parts[1];
+
+            if (command.equals("addBook")) {
+                String[] data = dataPart.split(",");
+                if (data.length != 2) {
+                    systemOut.println("Invalid command. Please try again.");
+                    continue;
+                }
+                String title = data[0];
+                String author = data[1];
+                addNewBook(title, author);
+            } else if (command.equals("addMoreBooks")) {
+                String[] data = dataPart.split(",");
+                if (data.length != 2) {
+                    systemOut.println("Invalid command. Please try again.");
+                    continue;
+                }
+                String bookID = data[0];
+                int count = Integer.parseInt(data[1]);
+                addMoreBooks(bookID, count);
+            } else if (command.equals("search")) {
+                String[] keywords = dataPart.split(",");
+                List<Book> searchResults = search(Arrays.asList(keywords));
+                systemOut.println("Search results:");
+                for (Book book : searchResults) {
+                    systemOut.println(book.getTitle() + " by " + book.getAuthor());
+                }
+            } else if (command.equals("removeAllBook")) {
+                String bookID = dataPart;
+                removeAllBook(bookID);
+            } else if (command.equals("removeKBooks")) {
+                String[] data = dataPart.split(",");
+                if (data.length != 2) {
+                    systemOut.println("Invalid command. Please try again.");
+                    continue;
+                }
+                String bookID = data[0];
+                int count = Integer.parseInt(data[1]);
+                removeKBooks(bookID, count);
+            } else {
+                systemOut.println("Unrecognized command.");
+            }
+        }
+    }
+
+    public void userProgram() {
+        systemOut.println("--- User side ---");
+        systemOut.println("search: search <keyword1>,<keyword2>,...,<keywordN>");
+        systemOut.println("reserve: reserve <bookID>");
+        systemOut.println("returnBook: returnBook <bookID>");
+        systemOut.println("exit: exit");
+
+        while (true) {
+            systemOut.print("Enter your command: ");
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.equals("exit")) {
+                systemOut.println("Bye!");
+                scanner.close();
+                return;
+            }
+
+            String[] parts = userInput.split(" ", 2);
+            if (parts.length != 2) {
+                systemOut.println("Invalid command. Please try again.");
+                continue;
+            }
+            String command = parts[0];
+            String dataPart = parts[1];
+
+            if (command.equals("search")) {
+                String[] keywords = dataPart.split(",");
+                List<Book> searchResults = search(Arrays.asList(keywords));
+                systemOut.println("Search results:");
+                for (Book book : searchResults) {
+                    systemOut.println(book.getTitle() + " by " + book.getAuthor());
+                }
+            } else if (command.equals("reserve")) {
+                String bookID = dataPart;
+                boolean reserveSuccess = reserve(bookID);
+                if (reserveSuccess) {
+                    systemOut.println("Book reserved successfully.");
+                } else {
+                    systemOut.println("Failed to reserve the book.");
+                }
+            } else if (command.equals("returnBook")) {
+                String bookID = dataPart;
+                boolean returnSuccess = returnBook(bookID);
+                if (returnSuccess) {
+                    systemOut.println("Book returned successfully.");
+                } else {
+                    systemOut.println("Failed to return the book.");
+                }
+            } else {
+                systemOut.println("Unrecognized command.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DatabaseManager databaseManager = new DatabaseManager();
+        SystemManager system = new SystemManager(databaseManager);
+        system.run();
+    }
 }
--- src/test/java/cs/nyuad/csuh3260/library/SystemManagerTest.java
+++ src/test/java/cs/nyuad/csuh3260/library/SystemManagerTest.java
@@ -1,14 +1,30 @@
 package cs.nyuad.csuh3260.library;
 
 import cs.nyuad.csuh3260.library.Book;
+import java.io.PrintStream;
 import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+
 public class SystemManagerTest {
+
     private SystemManager system;
     private DatabaseManager databaseManager;
 
@@ -105,5 +121,274 @@ public class SystemManagerTest {
         assertFalse(system.getAvailabilityList().containsKey("1"));
     }
     
+   
+    // @Test
+    // public void testSingupSuccessfully() {
+    //     when(databaseManager.getUsers()).thenReturn(new ArrayList<>());
+
+    //     boolean status = system.signup("a", "a", "a");
+    //     verify(databaseManager).addUser(any());
+    //     assertTrue(status);
+    // }
+
+    // @Test
+    // public void testSingupWithExistingUsername() {
+    //     List<User> users = List.of(new User("a", "a", "a"));
+    //     when(databaseManager.getUsers()).thenReturn(users);
+
+    //     boolean status = system.signup("a", "a", "a");
+    //     assertFalse(status);
+    // }
+
+    // @Test
+    // public void testLoginSuccessfully() {
+    //     List<User> users = List.of(new User("a", "a", "a"));
+    //     when(databaseManager.getUsers()).thenReturn(users);
+
+    //     boolean status = system.login("a", "a");
+    //     assertTrue(status);
+    // }
+
+    // @Test
+    // public void testLoginFail() {
+    //     List<User> users = List.of(new User("a", "a", "a"));
+    //     when(databaseManager.getUsers()).thenReturn(users);
+
+    //     boolean status = system.login("b", "b");
+    //     assertFalse(status);
+    // }
+
+    @Test
+    public void testAdminProgramExitCommand() {
+        // Mock dependencies
+        Scanner scanner = mock(Scanner.class);
+        SystemManager systemManager = new SystemManager(scanner);
+
+        // Simulate user input
+        when(scanner.nextLine()).thenReturn("exit");
+
+        // Execute method
+        systemManager.adminProgram();
+
+        // Verify behavior
+        verify(scanner).close();
+    }
+
     
+
+    @Test
+    public void testAddMoreBooksWhenAdminProgram() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        SystemManager systemManager = spy(new SystemManager(databaseManager, scanner, systemOut));
+
+        when(databaseManager.getBooks()).thenReturn(Collections.emptyList()); // Add this line
+
+        when(scanner.nextLine()).thenReturn("addBook 1,1")
+                .thenReturn("exit");
+
+        systemManager.adminProgram();
+        verify(systemManager).addNewBook("1", "1");
+
+        // Check for print when incorrect arguments
+        scanner = mock(Scanner.class);
+        systemOut = mock(PrintStream.class);
+        systemManager = new SystemManager(scanner, systemOut);
+
+        when(scanner.nextLine()).thenReturn("addMoreBooks 1,1,1")
+                .thenReturn("exit");
+
+        systemManager.adminProgram();
+        verify(systemOut).println("Invalid command. Please try again.");
+    }
+
+    @Test
+    public void testSearchWhenAdminProgram() {
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        SystemManager systemManager = spy(new SystemManager(databaseManager, scanner, systemOut));
+        
+        Book book1 = new Book("1", "Book 1", "Author 1", 1);
+        Book book2 = new Book("2", "Book 2", "Author 2", 1);
+        List<Book> allBooks = Arrays.asList(book1, book2);
+        when(databaseManager.getBooks()).thenReturn(allBooks);
+        
+        when(scanner.nextLine()).thenReturn("search 1,2")
+                .thenReturn("exit");
+        
+        systemManager.adminProgram();
+        
+        verify(systemManager).search(Arrays.asList("1", "2"));
+        verify(systemOut).println("Search results:");
+        verify(systemOut).println("Book 1 by Author 1");
+        verify(systemOut).println("Book 2 by Author 2");
+    }
+
+    
+    
+
+    @Test
+    public void testUnrecognizedCommandWhenAdminProgram() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        SystemManager systemManager = new SystemManager(scanner, systemOut);
+
+        when(scanner.nextLine()).thenReturn("removeAllSearch 1,1,1")
+                .thenReturn("exit");
+
+        systemManager.adminProgram();
+        verify(systemOut).println("Unrecognized command.");
+    }
+
+    @Test
+    public void testUserProgramWithIncorrectArguments() {
+        // Check for print when incorrect arguments
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        SystemManager systemManager = new SystemManager(scanner, systemOut);
+
+        when(scanner.nextLine()).thenReturn("addBook")
+                .thenReturn("exit");
+
+        systemManager.userProgram();
+        verify(systemOut).println("Invalid command. Please try again.");
+    }
+
+    @Test
+    public void testSearchWhenUserProgram() {
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        SystemManager systemManager = spy(new SystemManager(databaseManager, scanner, systemOut));
+        
+        Book book1 = new Book("1", "Book 1", "Author 1", 1);
+        Book book2 = new Book("2", "Book 2", "Author 2", 1);
+        List<Book> allBooks = Arrays.asList(book1, book2);
+        when(databaseManager.getBooks()).thenReturn(allBooks);
+        
+        when(scanner.nextLine()).thenReturn("search 1,2")
+                .thenReturn("exit");
+        
+        systemManager.userProgram();
+        
+        verify(systemManager).search(Arrays.asList("1", "2"));
+        verify(systemOut).println("Search results:");
+        verify(systemOut).println("Book 1 by Author 1");
+        verify(systemOut).println("Book 2 by Author 2");
+    }
+    
+
+    @Test
+    public void testReturnBookWhenUserProgram() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        User mockedUser = new User("1", "John", "john", "password");
+        SystemManager systemManager = spy(new SystemManager(databaseManager, scanner, systemOut));
+
+        systemManager.setCurUser(mockedUser); // Add this line
+
+        Book book = new Book("1", "Book 1", "Author 1", 1);
+        when(databaseManager.getBooks()).thenReturn(Collections.singletonList(book));
+
+        when(scanner.nextLine()).thenReturn("returnBook 1")
+                .thenReturn("exit");
+
+        systemManager.userProgram();
+        verify(systemManager).returnBook("1");
+    }
+
+    @Test
+    public void testUnrecognizedCommandWhenUserProgram() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        SystemManager systemManager = new SystemManager(scanner, systemOut);
+
+        when(scanner.nextLine()).thenReturn("command 1")
+                .thenReturn("exit");
+
+        systemManager.userProgram();
+        verify(systemOut).println("Unrecognized command.");
+    }
+
+    @Test
+    public void testLoginWhenRun() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        User mockedUser = new User("1", "1", "1");
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        when(databaseManager.getUsers()).thenReturn(List.of(mockedUser));
+        SystemManager systemManager = spy(new SystemManager(scanner, systemOut, databaseManager, mockedUser));
+
+        when(scanner.nextLine()).thenReturn("login 1,1")
+                .thenReturn("exit");
+
+        when(systemManager.login("1", "1")).thenReturn(true);
+
+        systemManager.run();
+        verify(systemManager).login("1", "1");
+
+        // Check for print when login with incorrect arguments
+        scanner = mock(Scanner.class);
+        systemOut = mock(PrintStream.class);
+        systemManager = new SystemManager(scanner, systemOut);
+
+        when(scanner.nextLine()).thenReturn("login 1,1,1")
+                .thenReturn("exit");
+
+        systemManager.run();
+        verify(systemOut).println("Invalid number of arguments. Please try again.");
+    }
+
+    @Test
+    public void testSignupWhenRun() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        User mockedUser = new User("1", "1", "1");
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        when(databaseManager.getUsers()).thenReturn(List.of(mockedUser));
+        SystemManager systemManager = spy(new SystemManager(scanner, systemOut, databaseManager, mockedUser));
+
+        when(scanner.nextLine()).thenReturn("signup 1,1,1")
+                .thenReturn("exit");
+
+        when(systemManager.signup("1", "1", "1")).thenReturn(true);
+
+        systemManager.run();
+        verify(systemManager).signup("1", "1", "1");
+    }
+
+    @Test
+    public void testUnrecognizedCommandWhenRun() {
+        Scanner scanner = mock(Scanner.class);
+        PrintStream systemOut = mock(PrintStream.class);
+        SystemManager systemManager = new SystemManager(scanner, systemOut);
+
+        when(scanner.nextLine()).thenReturn("command 1")
+                .thenReturn("exit");
+
+        systemManager.run();
+        verify(systemOut).println("Invalid command. Please try again.");
+    }
+
+    @Test
+    public void testAdminProgramWhenRun() {
+        Scanner scanner = mock(Scanner.class);
+        when(scanner.nextLine()).thenReturn("login admin,admin").thenReturn("exit");
+
+        PrintStream systemOut = mock(PrintStream.class);
+        DatabaseManager databaseManager = mock(DatabaseManager.class);
+        User admin = new User("admin", "admin", "admin");
+        when(databaseManager.getUsers()).thenReturn(List.of(admin));
+        SystemManager systemManager = spy(new SystemManager(scanner, systemOut, databaseManager, admin));
+
+        when(systemManager.login(anyString(), anyString())).thenReturn(true);
+
+        systemManager.run();
+        verify(systemManager).adminProgram();
+    }
+
 }
+
